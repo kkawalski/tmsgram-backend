52edab87834983db2fa2a51189b8f0eda9f18f49
Uladzislau Kavaliou <vladislav.kovalev.alekseevich@gmail.com>
2022-07-06 21:14:18 +0300

    Try env

--- config/settings.py
+++ config/settings.py
@@ -9,19 +9,22 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import environ
 import os
 from pathlib import Path
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY") or "super-secret-key"
+SECRET_KEY = env("SECRET_KEY") or "super-secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -91,11 +94,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME') or "postgres",
-        'USER': os.getenv('DB_USER') or "postgres",
-        'PASSWORD': os.getenv('DB_PASSWORD') or "postgres",
-        'HOST': os.getenv('DB_HOST') or "localhost",
-        'PORT': os.getenv('DB_PORT') or "5432"
+        'NAME': env('DB_NAME') or "postgres",
+        'USER': env('DB_USER') or "postgres",
+        'PASSWORD': env('DB_PASSWORD') or "postgres",
+        'HOST': env('DB_HOST') or "localhost",
+        'PORT': env('DB_PORT') or "5432"
     }
 }
 
